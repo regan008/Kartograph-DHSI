a913fb7633bb6584e9221f0d37a27dfcbcce9d50
Amanda Regan <regan008@cougars.csusm.edu>
2015-06-11 15:43:48 -0700

    new syntax

--- js/custom-map.js
+++ js/custom-map.js
@@ -6,12 +6,8 @@ $( document ).ready(function() {
                 stroke: '#aaa',
                 fill: '#f6f4f2'
             }
-        }),
-        map.addLayer('Gyms', {
-            styles: {
-                stroke: '#CC6633',
-                fill: '#CC6633'
-            }
+        }).addLayer('Gyms' {
+            name: 'gyms' }
         });
     });
 });
