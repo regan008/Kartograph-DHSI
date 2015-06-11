6d39613116c70d3fd87f34415b3dca70b526d4b0
Amanda Regan <regan008@cougars.csusm.edu>
2015-06-11 14:53:06 -0700

    changed layername

--- js/custom-map.js
+++ js/custom-map.js
@@ -1,11 +1,12 @@
 $( document ).ready(function() {
     var map = kartograph.map('#map');
-    map.loadMap('svg/boston.svg', function(){
-        map.addLayer('shapefile', {
+    map.loadMap('svg/satellite.svg', function(){
+        map.addLayer('gyms', {
             styles: {
                 stroke: '#aaa',
                 fill: '#f6f4f2'
             }
+        
         });
     });
 });
