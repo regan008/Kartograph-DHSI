b2e7d81974c87787a1734cfede2883ab02ae1f6f
Amanda Regan <regan008@cougars.csusm.edu>
2015-06-11 14:25:49 -0700

    trying diff layer name

--- js/custom-map.js
+++ js/custom-map.js
@@ -3,13 +3,9 @@ $( document ).ready(function() {
     map.loadMap('svg/boston.svg', function(){
         map.addLayer('gyms', {
             styles: {
-                stroke: '#aaa'
+                stroke: '#aaa',
+                fill: '#f6f4f2'
             }
-        });
- 
-        map.getLayer('gyms').style('fill', function(data) {
-            console.log(data);
-            return (data.name.charAt(0) === "A") ? '#5ED391' : '#f6f4f2';
         });
     });
 });
