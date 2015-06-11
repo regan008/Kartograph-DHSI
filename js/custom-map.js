d0e3aa8539f0a37fb2ba3000d087d13b422b5af2
Amanda Regan <regan008@cougars.csusm.edu>
2015-06-11 16:00:32 -0700

    switch order

--- js/custom-map.js
+++ js/custom-map.js
@@ -1,12 +1,12 @@
 $( document ).ready(function() {
     var map = kartograph.map('#map');
     map.loadMap('svg/boston.svg', function(){
-        map.addLayer('Wards2', {
+        map.addLayer('Gyms', {
             styles: {
                 stroke: '#aaa',
                 fill: '#f6f4f2'
             }
-        }).addLayer('Gyms', {
+        }).addLayer('Wards2', {
             styles: {
                 stroke: '#FF0000'
             }
