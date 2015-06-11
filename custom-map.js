3112b418d4dd1bd7b063c983292217d37b12a0e0
Amanda Regan <regan008@cougars.csusm.edu>
2015-06-11 12:38:35 -0700

    adding libraries and ward shapefiles

---
+++ custom-map.js
@@ -0,0 +1,15 @@
+$( document ).ready(function() {
+    var map = kartograph.map('#map');
+    map.loadMap('svg/world.svg', function(){
+        map.addLayer('mylayer', {
+            styles: {
+                stroke: '#aaa'
+            }
+        });
+ 
+        map.getLayer('mylayer').style('fill', function(data) {
+            console.log(data);
+            return (data.name.charAt(0) === "A") ? '#5ED391' : '#f6f4f2';
+        });
+    });
+});
