6fba8a761bcf9d5ff138aa4e7b5b041db25284cf
Amanda Regan <regan008@cougars.csusm.edu>
2015-06-11 14:24:12 -0700

    trying diff layer name

--- js/custom-map.js
+++ js/custom-map.js
@@ -1,13 +1,13 @@
 $( document ).ready(function() {
     var map = kartograph.map('#map');
-    map.loadMap('../svg/boston.svg', function(){
-        map.addLayer('mylayer', {
+    map.loadMap('svg/boston.svg', function(){
+        map.addLayer('gyms', {
             styles: {
                 stroke: '#aaa'
             }
         });
  
-        map.getLayer('mylayer').style('fill', function(data) {
+        map.getLayer('gyms').style('fill', function(data) {
             console.log(data);
             return (data.name.charAt(0) === "A") ? '#5ED391' : '#f6f4f2';
         });
